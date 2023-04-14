19398490b5dd7cdfe6a85ecbaf025c5c41de91b3
Yab1 <yabilisanu@gmail.com>
2023-04-14 17:22:19 +0300

    added the custom header to the Home component

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -3,18 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import ReviewDetails from '../screens/ReviewDetails';
+import Header from '../shared/header';
 
 const Stack = createNativeStackNavigator();
 
-function HomeStack() {
+function HomeStack({ navigation }) {
   return (
     <NavigationContainer independent={true}>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          // headerShown: false,
-        }}>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ header: () => <Header navigation={navigation} title="GameZone"/> }}
+        />
         <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
       </Stack.Navigator>
     </NavigationContainer>
